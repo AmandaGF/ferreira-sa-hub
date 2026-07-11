64ce5e63c326e0faa28f3ca55db0225bef4ef0b1
Amanda Guedes Ferreira <amandaguedesferreira@gmail.com>
2026-07-11 19:06:43 -0300

    chore: remove diags

--- ck.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (($_GET['key'] ?? '') !== 'fsa-hub-deploy-2026') { http_response_code(403); die('403'); }
-header('Content-Type: text/plain; charset=utf-8');
-$log = __DIR__ . '/error_log';
-if (is_file($log)) {
-    $c = @file_get_contents($log);
-    echo substr($c, -6000);
-} else echo "sem log";
