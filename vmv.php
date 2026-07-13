018e1a194acf3a57807a66f25a2898495d56f77d
Amanda Guedes Ferreira <amandaguedesferreira@gmail.com>
2026-07-13 10:53:58 -0300

    chore: remove diags salavip

--- vmv.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (($_GET['key'] ?? '') !== 'fsa-hub-deploy-2026') { http_response_code(403); die('403'); }
-header('Content-Type: text/plain; charset=utf-8');
-$f = dirname(__DIR__) . '/salavip/pages/mensagem_ver.php';
-echo "== $f ==\n\n";
-echo file_get_contents($f);
