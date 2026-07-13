1e91aeb2c86fae7abb8114ea947624258b9c9b5b
Amanda Guedes Ferreira <amandaguedesferreira@gmail.com>
2026-07-13 10:33:24 -0300

    chore: remove gk

--- gk.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (($_GET['key'] ?? '') !== 'fsa-hub-deploy-2026') { http_response_code(403); die('403'); }
-require_once __DIR__ . '/core/config.php';
-header('Content-Type: text/plain; charset=utf-8');
-echo "ANTHROPIC_API_KEY:\n";
-echo (defined('ANTHROPIC_API_KEY') ? ANTHROPIC_API_KEY : '(nao definida)') . "\n";
